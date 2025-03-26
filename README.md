a9fd9a5212075ea8e6c9c540cd3411c8da4ab26b
Peeradon Ruengkaew <76780514+peeradonmoke2002@users.noreply.github.com>
2025-03-26 13:07:26 +0700

    update more

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # FRA631_Project_Dual_arm_UR5
- 
+
+## Installation
+
+step 1 crate venv and activate it
+```bash
+set-executionpolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
+python -m venv venv
+venv/Scripts/Activate.ps1
+```
+step2 install requirements
+```bash
+pip3 install -r .\requirements.txt
+```
---
+++ test_connection.py
@@ -0,0 +1,26 @@
+import rtde.rtde as rtde
+import rtde.rtde_config as rtde_config
+
+ROBOT_IP = "192.168.200.10"
+PORT = 30004
+
+conf = rtde_config.ConfigFile("record_configuration.xml")  # XML defines what to read
+output_names, output_types = conf.get_recipe("out")
+
+con = rtde.RTDE(ROBOT_IP, PORT)
+con.connect()
+
+# Setup data output
+con.send_output_setup(output_names, output_types)
+
+# Start receiving data
+con.send_start()
+
+while True:
+    state = con.receive()
+    if state:
+        print("Joint positions:", state.actual_q)
+        print("Joint velocities:", state.actual_qd)
+        print("Joint currents:", state.actual_current)
+        print("Tool Accelerometer:", state.actual_TCP_force)
+        print("Tool Accelerometer:", state.actual_TCP_force)
